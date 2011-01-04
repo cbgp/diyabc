4aea11c7bc0c123a5ae48c1563445ce4da5924ec
Jean-Marie Cornuet <jmcornuet@googlemail.com>
2011-01-04 16:13:34 +0100

    shell

--- tests/main.py
+++ tests/main.py
@@ -5,7 +5,7 @@ from test import Ui_GroupBox
 import os
 
 class plop(QGroupBox):
-    """ comme  nt"""
+    """ commu  nt"""
     def __init__(self,parent=None):
         super(plop,self).__init__(parent)
         self.createWidgets()
