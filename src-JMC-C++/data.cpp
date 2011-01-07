4ca1f00e95029d9b420d0e133b13410e6bde17aa
Jean-Marie Cornuet <jmcornuet@googlemail.com>
2011-01-07 17:31:50 +0100

    data.cpp : récupération des noms de locus depuis le fichier genepop

--- src-JMC-C++/data.cpp
+++ src-JMC-C++/data.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 struct LocusC
 {
-	string name;
+	char* name;
 	int type;  //0 à 9
 	int groupe;    //numero du groupe auquel appartient le locus
 	double coeff;  // coefficient pour la coalescence (dépend du type de locus et du sexratio)
@@ -29,7 +29,7 @@ struct LocusC
 	int *sitmut;   //array of dna sites that are changed through a mutation
 	int dnalength;
 	int *tabsit;
-	int *haplostate; //array[gene copy] tous les "gene copies" sont mises à la suite les unes des autres (groupées par sample)
+//	int *haplostate; //array[gene copy] tous les "gene copies" sont mises à la suite les unes des autres (groupées par sample)
 	int **haplomic; //array[sample][gene copy]
 //Proprietes des locus microsatellites
 	int mini,maxi,kmin,kmax,motif_size,motif_range,nal;
@@ -64,36 +64,47 @@ struct DataC
 
 string majuscules(string s) {
 	string s2;
+	char *c;
+	c = new char[s.length()+1];
+	for (int i=0;i<s.length();i++) {
+		c[i]=s[i];
+		c[i]=toupper(c[i]);
+	}
+	s2 = string(c,s.length());
 	return s2;
 }
 
 DataC read_genepop(string filename) {
 	bool fin=false;
-	string s,s2;
-	char* s1;
+	string s,s1,s2,locusname;
 	char c;
-	int j;
+	size_t j;
 	DataC gpdata;
 	stringstream out;
-	//gpdata = new DataC;
 	gpdata.filename = filename;
+	cout << gpdata.filename<<"\n";
 	ifstream file(filename.c_str(), ios::in);
 	if (file == NULL) {
 		gpdata.message = "File not found";
 		return gpdata;
 	}
 	getline(file,gpdata.title);
+	cout <<gpdata.title<<"\n";
 	gpdata.nloc=0;
 	while (not fin) {
 		getline(file,s);
-		s=majuscules(s);
-		if (s.find("POP")==string::npos){	//il s'agit d'un nom de locus
+		s1=majuscules(s);
+		cout << s << "    " << s1 <<"\n";
+		if (s1.find("POP")==string::npos){	//il s'agit d'un nom de locus
 			gpdata.nloc +=1;
+			cout <<"nloc="<<gpdata.nloc<<"\n";
 			if (gpdata.nloc==1) gpdata.locus = (LocusC*) malloc(sizeof(LocusC));
 			else		        gpdata.locus = (LocusC*) realloc(gpdata.locus,gpdata.nloc*sizeof(LocusC));
+			cout <<"s = "<<s<< "\n";
 			j=s.find("<");
+			//cout <<"j="<<j<<"   npos="<<string::npos<<"\n";
 			if (j!=string::npos) {         //il y a un type de locus noté après le nom
-				s2=toupper(s[j+1]);
+				s2=majuscules(s.substr(j+1,1));
 				if (s2=="A") gpdata.locus[gpdata.nloc-1].type=0; else
 					if (s2=="H") gpdata.locus[gpdata.nloc-1].type=1; else
 						if (s2=="X") gpdata.locus[gpdata.nloc-1].type=2; else
@@ -105,9 +116,13 @@ DataC read_genepop(string filename) {
 									}
 				s=s.substr(0,j-1);
 			}
-			gpdata.locus[gpdata.nloc-1].name=s;
+			gpdata.locus[gpdata.nloc-1].name = new char[s.length()+1];
+			strncpy(gpdata.locus[gpdata.nloc-1].name, s.c_str(), s.length()+1);
+			cout << gpdata.locus[gpdata.nloc-1].name<<"\n";
 		} else fin=true;
 	}
+	gpdata.nsample=0;
+
 	return gpdata;
 }
 
