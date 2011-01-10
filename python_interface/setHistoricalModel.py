9f1ac1fab2536991a46908b7d5415f573b78ba28
Julien Veyssier <julien.veyssier@aiur.fr>
2011-01-10 17:22:50 +0100

    scenarios ok

--- python_interface/setHistoricalModel.py
+++ python_interface/setHistoricalModel.py
@@ -20,14 +20,11 @@ class setHistoricalModel(QMainWindow):
         self.ui.setupUi(self)
         QObject.connect(self.ui.addScButton,SIGNAL("clicked()"),self.addSc)
 
+        # liste des scenarios non effacés
         self.scList = []
 
     def addSc(self):
-        #ggg = QGroupBox("plop",self.ui.scScrollContent)
-        #self.ui.horizontalLayout_3.addWidget(ggg)
-
-        # ne pas oublier de connecter le bouton remove à la suppr
-
+        # creation de la groupbox a ajouter
         groupBox = QtGui.QGroupBox(self.ui.scScrollContent)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -39,8 +36,9 @@ class setHistoricalModel(QMainWindow):
         verticalLayout_6.setObjectName("verticalLayout_6")
         horizontalLayout_6 = QtGui.QHBoxLayout()
         horizontalLayout_6.setObjectName("horizontalLayout_6")
-        label_3 = QtGui.QLabel("scenario N",groupBox)
-        label_3.setObjectName("label_3")
+        label_3 = QtGui.QLabel("scenario "+str(len(self.scList)),groupBox)
+        # ne mettre qu'un seul label du nom scLabel pour pouvoir le retrouver avec findChild()
+        label_3.setObjectName("scLabel")
         horizontalLayout_6.addWidget(label_3)
         pushButton_6 = QtGui.QPushButton("remove",groupBox)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Fixed)
@@ -56,16 +54,25 @@ class setHistoricalModel(QMainWindow):
         plainTextEdit.setLineWrapMode(QtGui.QPlainTextEdit.NoWrap)
         plainTextEdit.setObjectName("plainTextEdit")
         verticalLayout_6.addWidget(plainTextEdit)
+        # ajout de la groupbox
         self.ui.horizontalLayout_3.addWidget(groupBox)
 
+        # evennement de suppression du scenario
         QObject.connect(pushButton_6,SIGNAL("clicked()"),self.rmSc)
 
         self.scList.append(groupBox)
 
     def rmSc(self):
-        print self.sender().objectName()
-        print "plop"
+        """ Suppression d'un scenario dans l'affichage et dans la liste locale
+        """
         self.sender().parent().hide()
+        self.scList.remove(self.sender().parent())
+        ## mise a jour des index dans le label
+        for i in range(len(self.scList)):
+            self.scList[i].findChild(QLabel,"scLabel").setText("scenario %i"% i)
+            # manière moins sure mais tout de même intéressante dans le principe
+            #self.scList[i].layout().itemAt(0).layout().itemAt(0).widget().setText("scenario %i"% i)
+
 
     def closeEvent(self, event):
         self.parent.setDisabled(False)
