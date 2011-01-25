80a332cb061be78d5b5434a61aacf0314fa27c65
Jean-Marie Cornuet <jmcornuet@googlemail.com>
2011-01-25 15:59:55 +0100

    creation de general.cpp

---
+++ src-JMC-C++/general.cpp
@@ -0,0 +1,17 @@
+/*
+ * general.cpp
+ *
+ *  Created on: 25 jan. 2010
+ *      Author: cornuet
+ */
+
+#include <iostream>
+#include "simuldatC.cpp"
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+}
