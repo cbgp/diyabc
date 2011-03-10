186d120edfd51e890ab1ffd1507bf7702773a75d
Julien Veyssier <julien.veyssier@aiur.fr>
2011-03-10 12:09:25 +0100

    recursion sur les possibilités de clics

--- python_interface/unit_test.py
+++ python_interface/unit_test.py
@@ -31,32 +31,91 @@ class testDiyabc(unittest.TestCase):
 
         project = self.tOpenExistingProject("/home/julien/vcs/git/diyabc/python_interface/datafiles/test/ploop_2011_1_18-3/")
 
-        firstClicks = [project.ui.setHistoricalButton, project.ui.setGeneticButton, project.ui.tabRefTable, project.ui.tabAnalyses]
+        
+        self.dico_names = {project.ui.setHistoricalButton : "setHistoricalButton",
+                project.ui.setGeneticButton : "setGeneticButton",
+                project.ui.tabRefTable : "tabRefTable",
+                project.ui.tabAnalyses :"tabAnalyses",
+                project.hist_model_win.ui.exitButton : "histExitButton",
+                project.hist_model_win.ui.clearButton : "histClearButton",
+                project.hist_model_win.ui.okButton : "histOkButton",
+                project.hist_model_win.ui.addScButton : "histAddScButton",
+                project.hist_model_win.ui.chkScButton : "histChkScButton",
+                project.hist_model_win.ui.defPrButton : "histDefPrButton",
+                0 : "root"
+                }
         self.dicoPossibleClicks = {}
+
+        root = 0
+        self.dicoPossibleClicks[root] = [project.ui.setHistoricalButton, project.ui.setGeneticButton, project.ui.tabRefTable, project.ui.tabAnalyses]
         self.dicoPossibleClicks[project.ui.setHistoricalButton] = [project.hist_model_win.ui.exitButton,
                                                               project.hist_model_win.ui.clearButton,
                                                               project.hist_model_win.ui.okButton,
-                                                              project.hist_model_win.ui.adScButton,
+                                                              project.hist_model_win.ui.addScButton,
                                                               project.hist_model_win.ui.chkScButton,
                                                               project.hist_model_win.ui.defPrButton
                                                               ]
-        self.dicoPossibleClicks[project.hist_model_win.ui.exitButton] = firstClicks
+        self.dicoPossibleClicks[project.hist_model_win.ui.exitButton] = self.dicoPossibleClicks[root]
         self.dicoPossibleClicks[project.hist_model_win.ui.clearButton] = self.dicoPossibleClicks[project.ui.setHistoricalButton]
-        self.dicoPossibleClicks[project.hist_model_win.ui.okButton] = firstClicks
-        self.dicoPossibleClicks[project.hist_model_win.ui.adScButton] = self.dicoPossibleClicks[project.ui.setHistoricalButton]
+        self.dicoPossibleClicks[project.hist_model_win.ui.okButton] = self.dicoPossibleClicks[root]
+        self.dicoPossibleClicks[project.hist_model_win.ui.addScButton] = self.dicoPossibleClicks[project.ui.setHistoricalButton]
         self.dicoPossibleClicks[project.hist_model_win.ui.chkScButton] = []
         self.dicoPossibleClicks[project.hist_model_win.ui.defPrButton] = self.dicoPossibleClicks[project.ui.setHistoricalButton]
+        self.dicoPossibleClicks[project.ui.setGeneticButton] = []
+        self.dicoPossibleClicks[project.ui.tabRefTable] = []
+        self.dicoPossibleClicks[project.ui.tabAnalyses] = []
         
-        self.exploreClick(firstClicks)
+        self.butCount = {}
+        #f = open("/tmp/plop","w")
+        #for l in self.buildClickLists(root):
+        #    f.write(str(l)+"\n\n")
+        #f.close()
+        l = self.buildClickLists(root,0)
+        print l
 
-    def exploreClick(self,lclicks):
-        if lclicks == []:
-            return
+    def buildDicoButtonFromName(self,project,name):
+        dico_but_from_names = {"setHistoricalButton" : project.ui.setHistoricalButton,
+                "setGeneticButton" :project.ui.setGeneticButton,
+                "tabRefTable" : project.ui.tabRefTable,
+                "tabAnalyses" : project.ui.tabAnalyses,
+                "histExitButton" : project.hist_model_win.ui.exitButton,
+                "histClearButton" : project.hist_model_win.ui.clearButton,
+                "histOkButton" : project.hist_model_win.ui.okButton,
+                "histAddScButton" : project.hist_model_win.ui.addScButton,
+                "histChkScButton" : project.hist_model_win.ui.chkScButton,
+                "histDefPrButton" : project.hist_model_win.ui.defPrButton,
+                "root" : 0
+                }
+        return dico_but_from_names
+
+    def buildClickLists(self,but,level):
+        if self.dicoPossibleClicks[but] == []:
+            #print "[%s] fin sur %s\n"%(level,self.dico_names[but])
+            return [[self.dico_names[but]]]
         else:
-            for c in lclicks:
-                QTest.mouseClick(c,Qt.LeftButton)
-                self.exploreClick(self.dicoPossibleClicks[c]
-                        #TODO
+            if but in self.butCount.keys():
+                self.butCount[but] += 1
+                #print "[%s] %s eme fois sur %s"%(level,self.butCount[but],self.dico_names[but])
+            else:
+                #print "[%s] premiere fois sur %s"%(level,self.dico_names[but])
+                self.butCount[but] = 1
+            if self.butCount[but] < 2:
+                lr = []
+                for c in self.dicoPossibleClicks[but]:
+                    #print "[%s] exploration de %s"%(level,self.dico_names[c])
+                    lexp = list(self.buildClickLists(c,level+1))
+                    for sublist in lexp:
+                        #print "[%s] j'ajoute une liste (%s) a mon resultat\n"%(level,sublist)
+                        new_l = [self.dico_names[but]]
+                        new_l.extend(list(sublist))
+                        lr.append(new_l)
+                        #print "[%s] lr : %s"%(level,lr)
+            else:
+                #print "[%s] trop de fois sur %s"%(level,self.dico_names[but])
+                lr = [[]]
+            return lr
+
+        #QTest.mouseClick(c,Qt.LeftButton)
 
 
     def testGeneral(self):
