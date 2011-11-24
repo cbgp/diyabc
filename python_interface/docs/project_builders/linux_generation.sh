3c2dc1076895915aab81f43dc6f16c533d450914
Julien Veyssier <julien.veyssier@aiur.fr>
2011-11-24 18:00:14 +0100

    premier jet linux generation pyinstaller
    --HG--
    rename : python_interface/docs/project_builders/windows_generation.sh => python_interface/docs/project_builders/linux_generation.sh

--- python_interface/docs/project_builders/linux_generation.sh
+++ python_interface/docs/project_builders/linux_generation.sh
@@ -2,15 +2,14 @@
 
 function printUsage(){
 echo "usage : 
-windows_generation.sh  path_to_pyinstaller.py  [path_to_icon.ico  output_path  path_to_main.py] 
+linux_generation.sh  path_to_pyinstaller.py  [output_path  path_to_main.py] 
 "
 }
 
 
 pyinst=$1
-icon=$2
-output=$3
-pysrc=$4
+output=$2
+pysrc=$3
 BUILDDATE=`date +%d-%m-%Y`
 
 if [ $# -eq 0 ] ; then
@@ -18,8 +17,7 @@ if [ $# -eq 0 ] ; then
     exit
 else
     if [ $# -eq 1 ]; then
-        icon="../icons/coccicon.ico"
-        output="./wingeneration_"+`date +%e-%m-%Y_%R`
+        output="./lingeneration_"+`date +%e-%m-%Y_%R`
         echo "Your application will be generated in $output . Press enter to continue..."
         read
         pysrc="../../diyabc.py"
@@ -32,7 +30,7 @@ VERSION="`head -n 1 $VERSIONFILE`"
 
 APPNAME=`basename $pysrc | cut -d . -f 1`
 
-TMPBUILD=/c/$APPNAME-tmp_build-$VERSION
+TMPBUILD=/tmp/$APPNAME-tmp_build-$VERSION
 mkdir $TMPBUILD
 SOURCEDIR=`dirname $pysrc`
 cp -r $SOURCEDIR/*.py $SOURCEDIR/clean.sh $SOURCEDIR/analysis $SOURCEDIR/uis $SOURCEDIR/utils $SOURCEDIR/summaryStatistics $SOURCEDIR/mutationModel $SOURCEDIR/historicalModel $SOURCEDIR/geneticData $TMPBUILD/
@@ -46,7 +44,7 @@ echo `cat $TMPBUILD/$APPNAME.py | grep VERSION`
 # nettoyage de la config de pyinstaller
 rm -f `dirname $pyinst`/config.dat
 # lancement de la generation
-python $pyinst $pysrctmp --onefile -w --icon="$icon" -o "$output"
+python $pyinst $pysrctmp -o "$output"
 
 # copy of needed images
 mkdir $output/dist/docs
@@ -54,5 +52,5 @@ cp -r $SOURCEDIR/docs/accueil_pictures $SOURCEDIR/docs/executables $SOURCEDIR/do
 cp -r $SOURCEDIR/uis $output/dist/
 rm -rf $TMPBUILD
 sleep 3
-mv $output/dist/$APPNAME.exe $output/dist/$APPNAME-$VERSION.exe
+mv $output/dist/$APPNAME $output/dist/$APPNAME-$VERSION
 mv $output/dist $output/$APPNAME-$VERSION
