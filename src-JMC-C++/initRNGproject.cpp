c0131cbf6bfe9f5f38d7c683ca604a52a038c356
Pierre Pudlo <Pierre.Pudlo@univ-montp2.fr>
2011-12-13 10:07:42 +0100

    Changement mineur dans initRNGproject.cpp

--- src-JMC-C++/initRNGproject.cpp
+++ src-JMC-C++/initRNGproject.cpp
@@ -49,12 +49,13 @@ int main(int argc, char *argv[]){
 	int num_threads = 1;
 
 	// récupération des paramètres de la ligne de commande
-	while((optchar = getopt(argc,argv,"p:c:s:t:f")) !=-1) {
+	while((optchar = getopt(argc,argv,"hp:c:s:t:f")) !=-1) {
 		  if (optarg!=NULL) soptarg = string(optarg);
 		  switch (optchar) {
 	        case 'h' :
 	        	cout << "Build independent parallel RNGs" << endl << endl;
 	        	cout << "USAGE :\n";
+	        	cout << "-h <print this help>\n";
 	            cout << "-p <directory of header.txt>\n";
 	            cout << "-c <number of nodes in the cluster>\n";
 	            cout << "-s <seed for the random generator>\n";
@@ -91,7 +92,8 @@ int main(int argc, char *argv[]){
 	string firstRNGfile = path + "RNG_state_0000.bin";
 	ifstream test_file(firstRNGfile.c_str(), ios::in);
 	if((test_file != NULL) && (force == false)){
-		cout << "Some files saving the RNG states already exist. Use -f if you want to overwrite it." << endl;
+		cout << "Some files saving the RNG states already exist." << endl
+		     << "Use -f if you want to overwrite it." << endl;
 		return 0;
 	}
 	test_file.close();
@@ -103,7 +105,7 @@ int main(int argc, char *argv[]){
 
 	// Création des états des RNGs indépendants
 	// Il y a autant de RNG que num_nodes * num_threads
-	mtss = get_mt_parameters_st(32, 521, 0, (num_nodes*num_threads)-1, 4172, &countRNG);
+	mtss = get_mt_parameters_st(32, 607, 0, (num_nodes*num_threads)-1, 4172, &countRNG);
 	// Fixons les graines
 	for (int i = 0; i < countRNG; ++i)
 		sgenrand_mt(i + seed, mtss[i]);
