14864cf0adf860708b5580eb0f49cb9611707275
Julien Veyssier <julien.veyssier@aiur.fr>
2012-01-16 23:58:18 +0100

    amelioration l2h

--- Notice-DIYABC-v2/gen_html_doc.sh
+++ Notice-DIYABC-v2/gen_html_doc.sh
@@ -1,5 +1,18 @@
+# cleaning destination directory
 rm -rf Notice_DIYABC_principal2
+
+# writing l2h config
 echo "\$DVIPSOPT = '';" > ~/.latex2html-init
 echo "1;" >>  ~/.latex2html-init
-cat Notice_DIYABC_principal.tex | grep -v sectionv | grep -v renewcommand | sed -e s/report/article/ > Notice_DIYABC_principal2.tex
+
+# modifiying header of tex file to help l2h
+echo "\documentclass [a4paper]{article}" > Notice_DIYABC_principal2.tex
+echo "\usepackage{html}
+%\begin{htmlonly}
+%\newenvironment{tabularx}[2]{\begin{tabular}{#2}}{\end{tabular}}
+%\end{htmlonly}" >> Notice_DIYABC_principal2.tex
+# avoiding mistakes on section numerotation
+cat Notice_DIYABC_principal.tex | grep -v sectionv | grep -v renewcommand | grep -v documentclass >> Notice_DIYABC_principal2.tex
+
+# processing latex2html convertion
 latex2html Notice_DIYABC_principal2.tex -split 0 -show_section_numbers
