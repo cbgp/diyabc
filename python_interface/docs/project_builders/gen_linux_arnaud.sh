366d668cbc0a75a386a60b9f77bc3c67aec4ef9f
Jean-Marie Cornuet <jmcornuet@googlemail.com>
2012-04-27 16:44:00 +0200

    modif gen arnaud

--- python_interface/docs/project_builders/gen_linux_arnaud.sh
+++ python_interface/docs/project_builders/gen_linux_arnaud.sh
@@ -1,3 +1,6 @@
 cd ~/diyabc/python_interface/docs/project_builders/
 ./linux_generation.sh ./pyinstaller_1743/pyinstaller.py /tmp/outtmp ../../diyabc.py
-zip -r diyabc_linux.zip /tmp/outtmp/diy*
+rm -rf /tmp/outtmp ~/diyabc_linux.zip
+cd /tmp/outtmp
+zip -r ~/diyabc/python_interface/docs/project_builders/diyabc_linux.zip diy*
+mv ~/diyabc/python_interface/docs/project_builders/diyabc_linux.zip ~/
