178ad3518ed574e93c087f43b284a98daf0e2905
Jean-Marie Cornuet <jmcornuet@googlemail.com>
2012-05-11 14:13:31 +0200

    zip arnaud

--- python_interface/docs/project_builders/gen_linux_arnaud.sh
+++ python_interface/docs/project_builders/gen_linux_arnaud.sh
@@ -1,6 +1,7 @@
 cd ~/diyabc/python_interface/docs/project_builders/
 ./linux_generation.sh ./pyinstaller_1743/pyinstaller.py /tmp/outtmp ../../diyabc.py
-rm -rf /tmp/outtmp ~/diyabc_linux.zip
+rm -rf  ~/diyabc_linux.zip
 cd /tmp/outtmp
 zip -r ~/diyabc/python_interface/docs/project_builders/diyabc_linux.zip diy*
 mv ~/diyabc/python_interface/docs/project_builders/diyabc_linux.zip ~/
+notify-send "Le zip a été généré dans /home/estoup"
