76c8df85e0a273fe4b5733a538ccc4b2cde0f418
Julien Veyssier <julien.veyssier@aiur.fr>
2013-01-04 13:06:56 +0100

    correction package mac

--- python_interface/docs/project_builders/package-mac-i386.sh
+++ python_interface/docs/project_builders/package-mac-i386.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-packageDir="~/lastDiyabc"
-diyabc_git="~/adocs/diyabc"
+packageDir="/Users/user/lastDiyabc"
+diyabc_git="/Users/user/adocs/diyabc"
 
 rm -rf $packageDir
 cd $diyabc_git
@@ -9,11 +9,11 @@ cd $diyabc_git
 git checkout .
 git pull --rebase 
 make clean
-make || echo compilation error ; exit
-cp $diyabc_git/src-JMC-C++/general.exe $diyabc_git/python_interface/docs/executables/diyabc-comput-mac-i386
+make 
+cp $diyabc_git/src-JMC-C++/general $diyabc_git/python_interface/docs/executables/diyabc-comput-mac-i386
 
 cd $diyabc_git/python_interface/docs/project_builders/
-./windows_generation.sh ./pyinstaller_1355/pyinstaller.py ../icons/coccicon.ico $packageDir ../../diyabc.py
+
 ./mac_generation.sh ./pyinstaller_1355/pyinstaller.py ../icons/coccicon.icns $packageDir  ../../diyabc.py
 
 
