b1df513cf387b1e909a35f320368e4a4a8603649
Julien Veyssier <julien.veyssier@aiur.fr>
2013-01-08 14:25:23 +0100

    packages mac ok

---
+++ python_interface/docs/project_builders/package-mac-x86.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# script à exécuter sur la machine virtuelle osxqt
+
+packageDir="/Users/user/lastDiyabc"
+diyabc_git="/Users/user/adocs/diyabc"
+
+rm -rf $packageDir
+cd $diyabc_git
+
+git checkout .
+git pull --rebase 
+make clean
+CCVERSION="" make 
+
+cp $diyabc_git/src-JMC-C++/general $diyabc_git/python_interface/docs/executables/diyabc-comput-mac-i386
+
+cd $diyabc_git/python_interface/docs/project_builders/
+
+./mac_generation.sh ./pyinstaller_1355/pyinstaller.py ../icons/coccicon.icns $packageDir  ../../diyabc.py
+
+
+
---
+++ python_interface/docs/project_builders/package-mac-x86_64.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# script à exécuter sur la machine de raph qui donne du x86_64
+
+packageDir="/Users/raph/Documents/julien/lastDiyabc"
+diyabc_git="/Users/raph/Documents/julien/diyabc"
+
+rm -rf $packageDir
+cd $diyabc_git
+
+git checkout .
+git pull --rebase 
+make clean
+CCVERSION="-fsf-4.7" make 
+
+cp $diyabc_git/src-JMC-C++/general $diyabc_git/python_interface/docs/executables/diyabc-comput-mac-i386
+
+cd $diyabc_git/python_interface/docs/project_builders/
+
+./mac_generation.sh ./pyinstaller_1355/pyinstaller.py ../icons/coccicon.icns $packageDir  ../../diyabc.py
+
+
+
