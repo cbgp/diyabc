3fa2eb60d532e0f9794c8c9152e3655b0a674dbf
Julien Veyssier <julien.veyssier@aiur.fr>
2013-01-09 10:38:20 +0100

    deux types de scripts packages : manuel et builder

---
+++ python_interface/docs/project_builders/package-mac-i386_builder.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# script à exécuter sur la machine virtuelle osxqt
+
+packageDir="./lastDiyabc"
+diyabc_git="../../../"
+
+rm -rf $packageDir
+cd $diyabc_git
+
+make clean
+CCVERSION="" make 
+
+cp ./src-JMC-C++/general ./python_interface/docs/executables/diyabc-comput-mac-i386
+
+cd ./python_interface/docs/project_builders/
+
+./mac_generation.sh ./pyinstaller_1355/pyinstaller.py ../icons/coccicon.icns $packageDir  ../../diyabc.py
+
+
+
