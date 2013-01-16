3771bdba46b2ec93e45e5b6a7ef710781e2383ac
Julien Veyssier <julien.veyssier@aiur.fr>
2013-01-16 13:42:38 +0100

    correction chemins pour buildbot

--- gui/build/debian/diyabc_2.0_all/usr/local/src/diyabc/utils/countlines.sh
+++
@@ -1,2 +0,0 @@
-cat *.py analysis/*.py geneticData/*.py historicalModel/*.py mutationModel/*.py summaryStatistics/*.py utils/autoPreferences.py utils/cbgpUtils.py  utils/trayIconHandler.py | grep -v "^$" | grep -vE "^ *#" | wc -l
-cat *.py analysis/*.py geneticData/*.py historicalModel/*.py mutationModel/*.py summaryStatistics/*.py utils/autoPreferences.py utils/cbgpUtils.py  utils/trayIconHandler.py | wc -l
