f6510c43fa92dd5f44708a6ce2d6656d7c722f06
Julien Veyssier <julien.veyssier@aiur.fr>
2013-01-18 14:35:11 +0100

    win lin mac gen datapath

--- gui/build/windows_generation.sh
+++ gui/build/windows_generation.sh
@@ -42,10 +42,10 @@ cp -r $SOURCEDIR/../data/ $TMPBUILD/
 cp -r $SOURCEDIR/../utils/ $TMPBUILD/src/
 pysrctmp=$TMPBUILD/src/`basename $pysrc`
 #sed -i "s/development\ version/$VERSION ($BUILDDATE)/g" $TMPBUILD/$APPNAME.py
-sed -i "s/development\ version/$VERSION/g" $TMPBUILD/src/dataPath.py
-sed -i "s/01\/01\/1970/$BUILDDATE/g" $TMPBUILD/src/dataPath.py
-echo "sed -i 's/development\ version/$VERSION ($BUILDDATE)/g' $TMPBUILD/src/dataPath.py"
-echo `cat $TMPBUILD/src/dataPath.py | grep VERSION`
+sed -i "s/development\ version/$VERSION/g" $TMPBUILD/src/variables.py
+sed -i "s/01\/01\/1970/$BUILDDATE/g" $TMPBUILD/src/variables.py
+echo "sed -i 's/development\ version/$VERSION ($BUILDDATE)/g' $TMPBUILD/src/variables.py"
+echo `cat $TMPBUILD/src/variables.py | grep VERSION`
 
 # nettoyage de la config de pyinstaller
 rm -f `dirname $pyinst`/config.dat
