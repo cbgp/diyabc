2daf983c4fdad98867f7a1fb7e80a00dcd2ae304
dehneg <dehneg@supagro.inra.fr>
2013-01-25 00:14:48 +0100

    add " around text variables in generation of first part of launch.sh in order to save spaces

--- gui/src/projectReftable.py
+++ gui/src/projectReftable.py
@@ -309,10 +309,10 @@ numSimulatedDataSetByJob={1}
 coresPerJob={2}
 maxConcurrentJobs={3}
 seed={4}
-diyabcPath={5}
-projectName={6}
-dataFileName={7}
-referenceTableHeaderName={8}
+diyabcPath="{5}"
+projectName="{6}"
+dataFileName="{7}"
+referenceTableHeaderName="{8}"
 """.format(numSimulatedDataSet, 
            numSimulatedDataSetByJob,
            coresPerJob,
