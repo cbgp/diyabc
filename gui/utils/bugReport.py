61426447eeda3a78065cb90cd8f91024027dbc49
Julien Veyssier <julien.veyssier@aiur.fr>
2013-02-10 23:50:54 +0100

    bug report http python aussi

---
+++ gui/utils/bugReport.py
@@ -0,0 +1,23 @@
+from mod_python import apache
+from mod_python import util
+import datetime,os
+
+def handler(req):
+        req.log_error('handler')
+        req.content_type = 'text/html'
+        req.send_http_header()
+        #req.write('<html><head><title>Testing mod_python</title></head><body>')
+        #req.write('%s'%util.FieldStorage(req)["file"])
+        try:    
+                date =  datetime.datetime.now().strftime("%Y %m %d %H:%M:%S")
+                ip = req.connection.remote_ip
+                name = "%s/%s %s.tar.gz"%(os.path.dirname(os.path.abspath(__file__)),date,ip)
+                f = open(name,"w")
+                f.write(util.FieldStorage(req)["file"])
+                f.close()
+                req.write('OK')
+        except Exception as e:
+                req.write('%s'%e)
+        #req.write('</body></html>')
+        return apache.OK
+
