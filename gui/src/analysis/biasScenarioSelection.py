955ab30cf68aa993bddf13a6d8063307ab70857c
Alexandre Dehne Garcia <dehneg@supagro.inra.fr>
2013-12-17 19:02:45 +0100

    remove stupid print test

--- gui/src/analysis/biasScenarioSelection.py
+++ gui/src/analysis/biasScenarioSelection.py
@@ -143,7 +143,6 @@ class BiasNConfidenceScenarioSelection(formBiasScenarioSelection,baseBiasScenari
                     #Quick and not so dirty patch to avoid prior config panel when confidence use posteriors
                     tmpWidget = HistDrawn(self.analysis,self.parent)
                     next_widget = tmpWidget.parent.parent.getNextWidget(tmpWidget)
-                    print "next widget 1 = " , next_widget
                     # skip also  SNP group selection
                     if self.parent.parent.isSnp() :
                         next_widget.validate()
